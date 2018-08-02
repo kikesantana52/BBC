dfb8ba201fa5c186a2bba7a61e10c44465465246
KikeOficial <kikesantana52@gmail.com>
2018-08-02 10:00:14 -0500

    COMMIT

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  $('.popovere').popover('enable')
+  $('.popovere').popover('enable');
+
+  $(".port-item").click(function(event) {
+    $('.collapse').collapse('hide');
+    $(".port-item").removeClass('activate-pass');
+    $(this).addClass('activate-pass')
+  });
+
+  $(".nav-paas").click(function(event) {
+    $('.paas-item').collapse('hide');
+    $(".port-item").removeClass('activate-pass');
+    bloque = $(this).data("bloque");
+    $("#bloque-" + bloque).addClass('activate-pass')  
+  });
 });
